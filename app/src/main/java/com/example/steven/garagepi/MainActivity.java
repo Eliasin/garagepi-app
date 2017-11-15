ac13fe2859ea3698e797b00546009ae1ffe211ce
Eliasin <steven.pham.8.2000@gmail.com>
2017-11-14 22:45:45 -0500

    Add ui prototypes

--- app/src/main/java/com/example/steven/garagepi/MainActivity.java
+++ app/src/main/java/com/example/steven/garagepi/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.steven.garagepi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void sendGarageToggleRequest(View V) {
+    }
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final View button = findViewById(R.id.button);
+        button.setOnClickListener(this::sendGarageToggleRequest);
+
+
+        final ListView deviceList = (ListView) findViewById(R.id.list);
+        final List<String> backingList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            backingList.add(Integer.toString(i));
+        }
+        ArrayAdapter<String> deviceListAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, backingList);
+        deviceList.setAdapter(deviceListAdapter);
     }
+
 }
