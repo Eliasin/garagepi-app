5ed256326b4b60f80738fe6ed6b86ab22173eedf
Eliasin <steven.pham.8.2000@gmail.com>
2017-11-15 19:31:51 -0500

    Add device list refresh functionality

--- app/src/main/java/com/example/steven/garagepi/MainActivity.java
+++ app/src/main/java/com/example/steven/garagepi/MainActivity.java
@@ -17,26 +17,33 @@ public class MainActivity extends AppCompatActivity {
     public void sendGarageToggleRequest(View v) {
     }
 
+    public List<String> getNearbyCompatibleDevices() {
+        return new ArrayList<>();
+    }
+
+    public void refreshDeviceList(View v) {
+        final ListView deviceList = (ListView) findViewById(R.id.device_list);
+        List<String> backingList = getNearbyCompatibleDevices();
+        ArrayAdapter<String> deviceListAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, backingList);
+        deviceList.setAdapter(deviceListAdapter);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final View button = findViewById(R.id.open_close_button);
-        button.setOnClickListener(this::sendGarageToggleRequest);
+        final View onOffButton = findViewById(R.id.open_close_button);
+        onOffButton.setOnClickListener(this::sendGarageToggleRequest);
+
+        final View refreshButton = findViewById(R.id.refresh_button);
+        refreshButton.setOnClickListener(this::refreshDeviceList);
 
         final ListView deviceList = (ListView) findViewById(R.id.device_list);
-        final List<String> backingList = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            backingList.add(Integer.toString(i));
-        }
-        ArrayAdapter<String> deviceListAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, backingList);
-        deviceList.setAdapter(deviceListAdapter);
         deviceList.setClickable(true);
-        deviceList.setOnItemClickListener((parent, view, position, id) -> {
-            selectedDeviceName = ((AppCompatTextView) view).getText().toString();
-        });
+        deviceList.setOnItemClickListener((parent, view, position, id) -> selectedDeviceName = ((AppCompatTextView) view).getText().toString());
+        refreshDeviceList(deviceList);
     }
 
 }
