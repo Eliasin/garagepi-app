4d84e9d9d733d7e5bd601a83b79cd9cbb58d137d
Eliasin <steven.pham.8.2000@gmail.com>
2017-11-19 14:58:49 -0500

    Fix device list refresh bug

--- app/src/main/java/com/example/steven/garagepi/MainActivity.java
+++ app/src/main/java/com/example/steven/garagepi/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.steven.garagepi;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatTextView;
@@ -16,15 +19,52 @@ public class MainActivity extends AppCompatActivity {
     private String selectedDeviceName = null;
     private List<String> deviceList = new ArrayList<>();
 
-    public void sendGarageToggleRequest(View v) {
+    private int BLUETOOTH_ENABLE_REQUEST = 1;
+
+    BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+    private void goToMainScreen() {
+        Intent startMain = new Intent(Intent.ACTION_MAIN);
+        startMain.addCategory(Intent.CATEGORY_HOME);
+        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(startMain);
     }
 
-    public List<String> getNearbyCompatibleDevices() {
-        return new ArrayList<>();
+    private void checkForBluetoothAdapter() {
+        if (bluetoothAdapter == null) {
+            System.err.println("No bluetooth adapter detected");
+            goToMainScreen();
+        }
+        else if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBluetoothIntent, BLUETOOTH_ENABLE_REQUEST);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == BLUETOOTH_ENABLE_REQUEST) {
+            if (resultCode == RESULT_CANCELED) {
+                System.err.println("Need bluetooth to function");
+                goToMainScreen();
+            }
+        }
+    }
+
+    private void sendGarageToggleRequest(View v) {
+    }
+
+    private List<String> getNearbyCompatibleDevices() {
+        List<String> names = new ArrayList<>();
+        for (BluetoothDevice bDevice : bluetoothAdapter.getBondedDevices()) {
+            names.add(bDevice.getName());
+        }
+        return names;
     }
 
     public void refreshDeviceList(View v) {
-        deviceList = getNearbyCompatibleDevices();
+        deviceList.clear();
+        deviceList.addAll(getNearbyCompatibleDevices());
         final ListView deviceListView = (ListView) findViewById(R.id.device_list);
         final BaseAdapter deviceListViewAdapter = (BaseAdapter) deviceListView.getAdapter();
         deviceListViewAdapter.notifyDataSetChanged();
@@ -35,6 +75,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkForBluetoothAdapter();
 
         final View onOffButton = findViewById(R.id.open_close_button);
         onOffButton.setOnClickListener(this::sendGarageToggleRequest);
